c9ab1624afdc1d069ab7db1d1c956b25fe034d48
castelldev <franklin.castellanos@asurion.com>
2024-06-07 13:57:37 -0400

    add missing nedb dependency

--- src/services/db/db.ts
+++ src/services/db/db.ts
@@ -1,9 +1,11 @@
-import Datastore from 'nedb'
 import * as path from 'path'
+import NeDb from 'nedb'
 
-const NeDb = window.require('nedb')
 const dbName = 'Plugin.request-navigator'
 const dbPath = path.join(window.app.getPath('userData'), `insomnia.${dbName}.db`)
-const databaseInternal = new NeDb({ filename: dbPath, autoload: true, corruptAlertThreshold: 0.9, inMemoryOnly: false }) as Datastore
-
-export const database = databaseInternal
+export const database = new NeDb({
+  filename: dbPath,
+  autoload: true,
+  corruptAlertThreshold: 0.9,
+  inMemoryOnly: false
+})
